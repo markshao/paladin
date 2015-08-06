f80360af7f6b90be667e99d63574bad1c5655f70
tim shen <tshen@splunk.com>
2015-08-06 11:23:30 +0800

    add some connection operation definations

--- orchestration/asynctasks.py
+++ orchestration/asynctasks.py
@@ -4,5 +4,105 @@ import logging
 logger = logging.getLogger("async_deploy_task")
 
 @task
-def a(x,y):
-    return x+y
+def initialize_master(node_id):
+    '''
+    initialize a node to be an index cluster master
+    :param node_id: the node selected to be the index master
+    :return:
+    '''
+    pass
+
+@task
+def connect_to_index_cluster(node_id, master_id):
+    '''
+    add a node to an index cluster, since master
+    :param node_id:  should be indexer, searchhead
+    :param master_id: the index cluster master
+    :return:
+    '''
+    pass
+
+@task
+def disconnect_from_index_cluster(node_id):
+    '''
+    remove a node from an index cluster, here just remove the config on that node,
+    so no need to know master
+    :param node_id: the node to be removed from index cluster
+    :return:
+    '''
+    pass
+
+@task
+def initialize_license_master(node_id):
+    '''
+    initialize a node to be a license master
+    :param node_id: the node selected to be the license master
+    :return:
+    '''
+    pass
+
+@task
+def connect_to_license_master(node_id, master_id):
+    '''
+    connect to a license master
+    :param node_id: the node needs to connect to a license master
+    :param master_id: the license master
+    :return:
+    '''
+    pass
+
+@task
+def disconnect_from_license_master(node_id):
+    '''
+    disconnect from a license master here just remove the config on that node,
+    so no need to know master
+    :param node_id: the node needs to disconnect from a license master
+    :return:
+    '''
+    pass
+
+@task
+def initialize_deployer(node_id):
+    '''
+    Initialize a node to be a deployer
+    :param node_id: the node to be a deployer
+    :return:
+    '''
+    pass
+
+@task
+def initialize_sh(sh_id, deployer_id):
+    '''
+    Initialize a search head, this includes connect to a deployer
+    :param sh_id:
+    :param deployer_id:
+    :return:
+    '''
+    pass
+
+@task
+def bootstrap_shc_captain(sh_id):
+    '''
+    Bootstrap a search head cluster captain
+    :param sh_id: the search head to be captain
+    :return:
+    '''
+    pass
+
+@task
+def add_search_peer(peer_id):
+    '''
+    Add a node as a search peer
+    :param peer_id: the search peer node to be added
+    :return:
+    '''
+    pass
+
+@task
+def remove_search_peer(peer_id):
+    '''
+    Remove a search peer
+    :param peer_id: the search peer node to be removed
+    :return:
+    '''
+    pass
